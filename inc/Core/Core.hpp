11bb761a48ffdc6e06d526765a033db9c8cf27e8
Malcret <t.marcopoulos@outlook.fr>
2022-10-11 16:18:43 +0200

    update

--- inc/Core/Core.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Core/Logger.hpp>
-#include <string>
-
-#define ASSERT(x) if (!x) { LOG_ERROR("{0}:{1}: Assertion failed !", __FILE__, __LINE__); abort(); }
-#define ASSERT_MSG(x, msg) if (!x) { LOG_ERROR("{0}:{1}: Assertion failed: {2}", __FILE__, __LINE__, msg); abort(); }
-#define ASSERT_QUIET(x) if (!x) { abort(); }
